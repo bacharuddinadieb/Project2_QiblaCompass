5f161cabc49104c229339dea799690cf76cda9ce
adieb.co.id@gmail.com <rahasia12345>
2017-03-29 21:34:06 +0700

    Innitial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl50714193336/qiblacompass/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project2.xirpl50714193336.qiblacompass;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
